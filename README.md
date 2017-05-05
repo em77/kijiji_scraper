832e1a925ca5d02b43fad02791d714274d148415
Eric M <ericmarchese77@gmail.com>
2017-05-04 21:47:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Instant Kijiji Listing Notifier
+#### Kijiji Alerts aren't fast enough to beat the crowd. When every minute counts, run this program on your server to refresh that Kijiji search page for you and e-mail you when new listings appear for your search.
+
+### Sign up with Sendgrid
+With some modification, you could use a different e-mail provider, I chose Sendgrid. [Sign up for free](http://sendgrid.com/) and get your API key.
+
+### Get your search URL
+Contruct the search you want on [Kijiji](http://www.kijiji.ca/). Be sure it is ordered "Newest first" and copy the URL.
+
+### Set your environment variables
+The following environment variables must be set:
+_FROM_ADDRESS_: The e-mail address the e-mail will be addressed from.
+_TO_ADDRESS_: The e-mail address the e-mail will be addressed to.
+_SUBJECT_: The subject of the e-mail.
+_SENDGRID_KEY_: Your Sendgrid API key.
+_SEARCH_URL_: Your search URL.
+
+### Run on your server
+It's ready to run on your server. Be sure it's set to respawn if necessary.
+
+### Additional info
+Two files will be created when running `scraper.rb`. `last_href_file.txt` will store the last URL to be posted for your search so that the program can be restarted without losing track of the most recent URL e-mailed to you. `email_log.txt` will keep a log of Sendgrid's responses for debugging purposes.
---
+++ scraper.rb
@@ -0,0 +1,68 @@
+require "nokogiri"
+require "open-uri"
+require "sendgrid-ruby"
+include SendGrid
+
+def replace_file_contents(string, file_name)
+  file = File.new(file_name, "w")
+  file.print(string)
+  file.close
+end
+
+def write_to_file(string, file_name)
+  file = File.new(file_name, "a")
+  file.print(string)
+  file.close
+end
+
+def send_email(from_address, to_address, subject_string, body_string, sg_key)
+  from = Email.new(email: from_address)
+  to = Email.new(email: to_address)
+  content = Content.new(type: "text/plain", value: body_string)
+  mail = Mail.new(from, subject_string, to, content)
+
+  sg = SendGrid::API.new(api_key: sg_key)
+  response = sg.client.mail._("send").post(request_body: mail.to_json)
+  log = "\n\n#{response.status_code}\n#{response.body}\n#{response.headers}\n\n"
+  write_to_file(log, "email_log.txt")
+end
+
+def search_urls(search_url, last_href)
+  doc = Nokogiri::HTML(open(search_url))
+  search_items = doc.css("div.regular-ad")
+  a_list = search_items.css("div.title a")
+  urls = []
+  a_list.each do |a|
+    break if a["href"] == last_href
+    urls << a["href"]
+  end
+  urls
+end
+
+def url_prepender(domain_to_prepend, url_string)
+  domain_to_prepend + url_string
+end
+
+domain = "http://www.kijiji.ca"
+
+loop do
+  # Randomize delay between searches
+  sleep [1, 2, 3].sample
+
+  urls = search_urls(ENV["SEARCH_URL"], File.readlines("last_href_file.txt").join)
+
+  next if urls.empty?
+
+  replace_file_contents(urls.first, "last_href_file.txt")
+
+  urls.collect! {|url| url_prepender(domain, url)}
+
+  send_email(ENV["FROM_ADDRESS"],
+             ENV["TO_ADDRESS"],
+             ENV["SUBJECT"],
+             urls.join("\n\n"),
+             ENV["SENDGRID_KEY"])
+
+  # Test e-mail body output in terminal
+  # puts urls.join("\n")
+end
